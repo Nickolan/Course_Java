3ce842e9452cb5d21c6006d47276d91bbaff23bf
Nicolas <nicolassantiagonavarrete.nsn@gmail.com>
2023-09-26 11:47:38 -0300

    nestedLoops fix

--- src/Sesion5/NestedLoops/NestedLoops.java
+++ src/Sesion5/NestedLoops/NestedLoops.java
@@ -13,14 +13,28 @@ public class NestedLoops {
         }
         System.out.println("Sum is " + sum);
 
+        System.out.println("CONTINUE");
         // ---- Uso Continue
         // imprimiendo el elemento de cada elemento
         int [][] values2 = {{1, 2, 3}, {4, 5}, {6}, {7, 8, 9}};
 
-        for (int i = 0; i<values2.length; i++){
-            for (int j = 0; j<values2[i].length; j++){
-                if (j>0) continue;
+        out2:
+        outfor: for (int i = 0; i<values2.length; i++){
+            innfor: for (int j = 0; j<values2[i].length; j++){
+                if (j >0) continue outfor;
                 System.out.println(values2[i][j]);
+            }
+        }
+
+        System.out.println("BREAK");
+        // ---- Uso Break
+        int[][] values3 = {{1, 2, 3}, {4, 5}, {6}, {7, 8, 9}};
+        OUT:{
+            OUTER: for (int i = 0; i<values3.length; i++){
+                for (int j=0; j<values3[i].length; j++){
+                    if(j>0) break OUT;
+                    System.out.println(values3[i][j]);
+                }
             }
         }
 
